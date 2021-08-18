44e0e6b14ec877ae0a456de747dc66e191d9f534
mxlle <almut.m.f@gmail.com>
2021-08-18 21:03:47 +0200

    Add prettier

--- src/app/scene.js
+++ src/app/scene.js
@@ -1,19 +1,19 @@
 export function addBackgroundScene() {
-    for (let i = 0; i < 1000; i++) {
-        addOneStarRow();
-    }
+  for (let i = 0; i < 1000; i++) {
+    addOneStarRow();
+  }
 }
 
-const text1 = " ★ ° . .　 　 .　☾ °☆ 　.  * ¸ 　 . 　 .　 . ● 　 　 ★ 　° : .　 . 　 •  ○ ° ★　 .  　 * 　. . 　 ° ";
-const text2 = ". .　  　 :.　 . • ° ★　 . 　 * .  ★ ° 　 　.　 ☾ ° ☆ 　. *  ¸ .　 　 　 　. 　 ° 　. ● ★ ° ";
-const text3 = "　 ○ ° ★ 　 .　  *　. 　 　 . 　 　 . 　 　 　. ★ ° . .　  .　 ☾ °☆ 　. * 　  ¸ .　  　 ★ 　 ° : . 　 . • 　 ° 　 . ● ";
+const text1 = ' ★ ° . .　 　 .　☾ °☆ 　.  * ¸ 　 . 　 .　 . ● 　 　 ★ 　° : .　 . 　 •  ○ ° ★　 .  　 * 　. . 　 ° ';
+const text2 = '. .　  　 :.　 . • ° ★　 . 　 * .  ★ ° 　 　.　 ☾ ° ☆ 　. *  ¸ .　 　 　 　. 　 ° 　. ● ★ ° ';
+const text3 = '　 ○ ° ★ 　 .　  *　. 　 　 . 　 　 . 　 　 　. ★ ° . .　  .　 ☾ °☆ 　. * 　  ¸ .　  　 ★ 　 ° : . 　 . • 　 ° 　 . ● ';
 
 function addOneStarRow() {
-    const textNode = document.createTextNode(getRandomText());
-    document.getElementById("sky").appendChild(textNode);
+  const textNode = document.createTextNode(getRandomText());
+  document.getElementById('sky').appendChild(textNode);
 }
 
 function getRandomText() {
-    const x = Math.random();
-    return x > 0.667 ? text1 : x > 0.333 ? text2 : text3;
+  const x = Math.random();
+  return x > 0.667 ? text1 : x > 0.333 ? text2 : text3;
 }
