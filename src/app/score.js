7a9a03a3a93fe179479d319804a70aeb4e407e9e
Almut Kieffer-Jones <almut@studysmarter.de>
2021-08-18 21:03:47 +0200

    Add prettier

--- src/app/score.js
+++ src/app/score.js
@@ -4,46 +4,48 @@ let score = 0;
 let victoryInPosition = false;
 
 export function checkWinningCondition() {
-    const cat = document.getElementById("cat");
-    const synth = document.getElementById("synth");
-    const catBox = cat.getBoundingClientRect();
-    const synthBox = synth.getBoundingClientRect();
+  const cat = document.getElementById('cat');
+  const synth = document.getElementById('synth');
+  const catBox = cat.getBoundingClientRect();
+  const synthBox = synth.getBoundingClientRect();
 
-    const overlap = !(catBox.right < synthBox.left ||
-        catBox.left > synthBox.right ||
-        catBox.bottom < synthBox.top ||
-        catBox.top > synthBox.bottom);
+  const overlap = !(
+    catBox.right < synthBox.left ||
+    catBox.left > synthBox.right ||
+    catBox.bottom < synthBox.top ||
+    catBox.top > synthBox.bottom
+  );
 
-    if (overlap) {
-        document.body.classList.add("victory");
-    } else {
-        document.body.classList.remove("victory");
-    }
+  if (overlap) {
+    document.body.classList.add('victory');
+  } else {
+    document.body.classList.remove('victory');
+  }
 
-    if (overlap && !victoryInPosition) {
-        score++;
-        victoryInPosition = true;
-        updateDisplayedScore();
-    }
+  if (overlap && !victoryInPosition) {
+    score++;
+    victoryInPosition = true;
+    updateDisplayedScore();
+  }
 }
 
 export function updateDisplayedScore() {
-    let highScore = getHighScore();
-    if (score > highScore) {
-        highScore = score;
-        saveHighScore(highScore);
-    }
-    document.getElementById("score").innerHTML = `Current&nbsp;Score:&nbsp;${score}<br/>High&nbsp;Score:&nbsp;${highScore}`;
+  let highScore = getHighScore();
+  if (score > highScore) {
+    highScore = score;
+    saveHighScore(highScore);
+  }
+  document.getElementById('score').innerHTML = `Current&nbsp;Score:&nbsp;${score}<br/>High&nbsp;Score:&nbsp;${highScore}`;
 }
 
 export function resetVictoryInPosition() {
-    victoryInPosition = false;
+  victoryInPosition = false;
 }
 
 function getHighScore() {
-    return Number(localStorage.getItem(HIGH_SCORE_KEY));
+  return Number(localStorage.getItem(HIGH_SCORE_KEY));
 }
 
 function saveHighScore(score) {
-    localStorage.setItem(HIGH_SCORE_KEY, score);
+  localStorage.setItem(HIGH_SCORE_KEY, score);
 }
--- src/index.js
+++ src/index.js
@@ -1,77 +1,76 @@
-import {addBackgroundScene} from "./app/scene.js";
-import {checkWinningCondition, resetVictoryInPosition, updateDisplayedScore} from "./app/score.js";
+import { addBackgroundScene } from './app/scene.js';
+import { checkWinningCondition, resetVictoryInPosition, updateDisplayedScore } from './app/score.js';
 
 let intervalId;
 let intervalCount = 0;
 
-
 function init() {
-    addBackgroundScene();
+  addBackgroundScene();
 
-    document.addEventListener("click", function(event){
-        console.log(event);
-        const cat = document.getElementById("cat");
-        cat.style.left = event.pageX + "px";
-        cat.style.top = event.pageY + "px";
-        if (Math.random() > 0.2) {
-            synthFleesToOtherPosition();
-        }
-        startIntervalChecking();
+  document.addEventListener('click', function (event) {
+    console.log(event);
+    const cat = document.getElementById('cat');
+    cat.style.left = event.pageX + 'px';
+    cat.style.top = event.pageY + 'px';
+    if (Math.random() > 0.2) {
+      synthFleesToOtherPosition();
+    }
+    startIntervalChecking();
 
-        if (event.target.id === 'space-key') {
-            toggleGlobalSpace();
-        }
-    });
+    if (event.target.id === 'space-key') {
+      toggleGlobalSpace();
+    }
+  });
 
-    document.addEventListener("keydown", function(event){
-        if (event.code === "Space"){
-            const cat = document.getElementById("cat");
-            cat.classList.add("animated");
-        }
-    });
+  document.addEventListener('keydown', function (event) {
+    if (event.code === 'Space') {
+      const cat = document.getElementById('cat');
+      cat.classList.add('animated');
+    }
+  });
 
-    document.addEventListener("keyup", function(event){
-        if (event.code === "Space"){
-            const cat = document.getElementById("cat");
-            cat.classList.remove("animated");
-        }
-    });
+  document.addEventListener('keyup', function (event) {
+    if (event.code === 'Space') {
+      const cat = document.getElementById('cat');
+      cat.classList.remove('animated');
+    }
+  });
 }
 
 function startIntervalChecking() {
-    intervalCount = 0;
+  intervalCount = 0;
 
-    if (!intervalId) {
-        intervalId = setInterval(() => {
-            checkWinningCondition();
-            intervalCount++;
-            if (intervalCount >= 20) {
-                clearInterval(intervalId);
-                intervalId = undefined;
-            }
-        }, 100);
-    }
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      checkWinningCondition();
+      intervalCount++;
+      if (intervalCount >= 20) {
+        clearInterval(intervalId);
+        intervalId = undefined;
+      }
+    }, 100);
+  }
 }
 
 function synthFleesToOtherPosition() {
-    const synth = document.getElementById("synth");
-    synth.style.left = getRandomInt(0, window.innerWidth) + "px";
-    synth.style.top = getRandomInt(0, window.innerHeight) + "px";
-    resetVictoryInPosition();
+  const synth = document.getElementById('synth');
+  synth.style.left = getRandomInt(0, window.innerWidth) + 'px';
+  synth.style.top = getRandomInt(0, window.innerHeight) + 'px';
+  resetVictoryInPosition();
 }
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function toggleGlobalSpace() {
-    if (document.body.classList.contains('global-space-animation')) {
-        document.body.classList.remove('global-space-animation');
-    } else {
-        document.body.classList.add('global-space-animation');
-    }
+  if (document.body.classList.contains('global-space-animation')) {
+    document.body.classList.remove('global-space-animation');
+  } else {
+    document.body.classList.add('global-space-animation');
+  }
 }
 
 // START
