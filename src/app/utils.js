2377d1d342cac492a7b24260314095bb84dd8bc0
Almut Kieffer-Jones <almut@studysmarter.de>
2021-08-30 22:53:20 +0200

    Some css refactoring for smaller size

---
+++ src/app/utils.js
@@ -0,0 +1,7 @@
+export function addBodyClasses(...classes) {
+  document.body.classList.add(...classes);
+}
+
+export function removeBodyClasses(...classes) {
+  document.body.classList.remove(...classes);
+}
