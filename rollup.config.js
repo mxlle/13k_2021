29d593766d5e7d6e529fd957a12328634d656df7
mxlle <almut.m.f@gmail.com>
2021-09-05 17:56:45 +0200

    Switch from webpack to rollup

---
+++ rollup.config.js
@@ -0,0 +1,56 @@
+import commonjs from 'rollup-plugin-commonjs';
+import htmlBundle from 'rollup-plugin-html-bundle';
+import kontra from 'rollup-plugin-kontra';
+import livereload from 'rollup-plugin-livereload';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import serve from 'rollup-plugin-serve';
+import styles from 'rollup-plugin-styles';
+import { terser } from 'rollup-plugin-terser';
+
+const production = !process.env.ROLLUP_WATCH;
+const outputDir = production ? 'dist' : 'out';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: `${outputDir}/space-cat.js`,
+    name: 'SpaceCat',
+    format: 'iife',
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    kontra({
+      gameObject: {
+        anchor: true,
+        group: true,
+        opacity: true,
+        rotation: true,
+        scale: true,
+        velocity: true,
+      },
+    }),
+    styles(),
+    production &&
+      terser({
+        mangle: {
+          properties: {
+            keep_quoted: true,
+          },
+        },
+        compress: {
+          booleans_as_integers: true,
+          drop_console: true,
+        },
+      }),
+    htmlBundle({
+      template: 'src/index.html',
+      target: `${outputDir}/index.html`,
+    }),
+    !production && serve({ contentBase: outputDir, open: true }),
+    !production && livereload(),
+  ],
+  watch: {
+    exclude: ['node_modules/**'],
+  },
+};
